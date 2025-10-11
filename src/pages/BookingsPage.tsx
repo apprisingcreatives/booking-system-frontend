b631ae9947d594043bfa34eedb25ea940b1ad256
gabriel gonzales <gabrielgnzls94@gmail.com>
2025-10-11 17:49:38 +0800

    initial commit

--- src/pages/BookingsPage.tsx
+++ src/pages/BookingsPage.tsx
@@ -2,7 +2,7 @@ import { Booking } from '../components';
 
 const BookingsPage = () => {
   return (
-    <div className='min-h-screen bg-gradient-to-br from-green-50 via-white to-blue-50'>
+    <div className='min-h-screen bg-gradient-to-br from-green-50 via-white to-blue-50 '>
       <Booking />
     </div>
   );
